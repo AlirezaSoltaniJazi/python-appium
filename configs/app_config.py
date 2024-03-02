da665bbdc15d023840e06a259f24b226ffbace64
Alireza Soltani <alireza.soltani.jazi@gmail.com>
2024-03-02 16:43:17 +0330

    Add app config

--- configs/app_config.py
+++ configs/app_config.py
@@ -5,18 +5,31 @@ class AppConfig:
     FIREFOX_CONFIG: Final[dict] = {
         "automationName": "UiAutomator2",
         "platformName": "Android",
+        "appium:platformVersion": "9.0",
         'appPackage': 'org.mozilla.firefox',
         'appActivity': 'org.mozilla.fenix.HomeActivity',
-        'device_name': 'emulator-5556'
+        # 'device_name': 'emulator-5556',
+        "appium:grantPermissions": True,
+        "appium:ensureWebviewsHavePages": True,
+        "appium:nativeWebScreenshot": True,
+        "appium:newCommandTimeout": 3600,
+        "appium:connectHardwareKeyboard": True,
+        "resetKeyboard": True
     }
     CHROME_CONFIG: Final[dict] = {
         "automationName": "UiAutomator2",
         "platformName": "Android",
+        "appium:platformVersion": "10.0",
         'appPackage': 'com.android.chrome',
         'appActivity': 'org.chromium.android_webview.devui.MainActivity',
-        'device_name': 'emulator-5556'
+        # 'device_name': 'emulator-5556',
+        "appium:grantPermissions": True,
+        "appium:ensureWebviewsHavePages": True,
+        "appium:nativeWebScreenshot": True,
+        "appium:newCommandTimeout": 3600,
+        "appium:connectHardwareKeyboard": True,
+        "resetKeyboard": True
     }
-
 
 
 class AppServer:
